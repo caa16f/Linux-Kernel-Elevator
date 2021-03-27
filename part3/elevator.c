ea960224385f2d7a3449fb8de7948a8bcc38147b
Brittany <brittanyzabawa123@gmail.com>
2021-03-27 17:23:04 -0400

    update elevator.c, but needs debugging in either makefile, wrappers.c, elevator.c

--- part3/elevator.c
+++ part3/elevator.c
@@ -10,11 +10,13 @@
 #include <linux/uaccess.h>
 #include <linux/string.h>
 #include <linux/list.h>
+#include <linux/linkage.h>
 #include <linux/fault-inject.h>
 #include <linux/gfp.h>
 
-MODULE_LICENSE("Dual BSD/GPL");
-MODULE_AUTHOR ("CARLOS ALFONSO");
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR ("CARLOS ALFONSO, BRITTANY ZABAWA");
 MODULE_DESCRIPTION("Elevator Scheduler");
 
 typedef enum {IDLE,UP,DOWN,LOADING,OFFLINE} movement_type;
@@ -63,28 +65,129 @@ static elevator_type elevator;
 static building_type building;
 static passenger_type * passenger;
 //Continue adding here
-static struct task_struct *thread_elevator;
+static struct task_struct *thread_elevator; //stores the kthread running the elevaotr's active
 static struct task_struct *thread_loader;
 
 static struct mutex elevator_mutex;
 static struct mutex building_mutex;
-
+static struct mutex floor_mutex;
 static char * currentMsg;
 static char * printMovement;
 
 static int msgLen;
 static int read_p;
 
+////////////////////////////////////////////
+
+static int currentFloor = 1; //Keeping track of floor member variable
+							 //is set as one just for testing basic up movement from elevator
+
+
+////////////////////////////////////////
+
+
+void moveUP(void)
+{
+	ssleep(2);
+	mutex_lock(&elevator_mutex);
+	currentFloor++;
+	//printk(KERN_ALERT "The elevator is at floor %d\n", currentFloor);
+	mutex_unlock(&elevator_mutex);
+	
+	
+}
+
+
+
+
+int runElevator(void * data)
+{
+
+	//Basic running through Elevator up
+	//Will run from first floor to top floor
+	int i;
+	for( i = 1; i <= 10; i++)
+	{
+	mutex_unlock(&elevator_mutex);
+	printk(KERN_ALERT "[]Floor %d\n" , i);
+	moveUP();
+	mutex_lock(&elevator_mutex);
+	}
+	mutex_unlock(&elevator_mutex);
+	
+	return 0;
+	
+	
+	
+	
+	
+	//TODO: basic running though Elevator down
+	
+
+
+
+
+
+
+	/*if(elevaor.movement == IDLE)
+	{
+		//TODO: Make condition when elevator is in IDLE, and look through every floor
+		//to see if anyone is waiting
+
+	}
+	else
+	{
+		
+		
+	}*/
+}
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
 extern long (*STUB_start_elevator)(void);
 long start_elevator(void){
 	printk("Starting Elevator\n");
+	
+	mutex_lock(&building_mutex); //not really sure what this does exactly unused
+	mutex_lock(&elevator_mutex);
+	
+	//if elevator is already active 
+	if(elevator.movement != OFFLINE)
+	{
+		mutex_unlock(&elevator_mutex); //let it continue to run there threads
+		mutex_unlock(&building_mutex);
+		return 1;
+	}
+	
+	
+	//TO DO: // set up default of carry items and passagers
+	// Meaning: 10 passangers and 10 carry items 
+	
+	
+	
 
-	elevator.movement = OFFLINE;
-	elevator.floor = 1;
-	elevator.targetFloor = 1;
-	elevator.load = 0;
-	elevator.occupancy =0;
+	elevator.movement = IDLE; // current state
+	elevator.floor = 1; //current floor
+	elevator.targetFloor = 1; //destination floor
+	elevator.occupancy =0; //number of people in elevator
 	elevator.shutdown =0;
+	
+	mutex_unlock(&elevator_mutex);
+	printk(KERN_INFO "STARTING ELEVATOR THREAD\n" );
+	
+	//TO DO: running elevator function
+	//This is basic testing for up motion of elevator
+	thread_elevator = kthread_run(runElevator, NULL, "ELEVATOR SIMIULATION");
+	printk(KERN_INFO "STARTING BUILDING THREAD\n" ); 
+	mutex_unlock(&building_mutex);
+	
+	
+	
 
 	return 0;
 
@@ -128,15 +231,30 @@ static int elevator_init(void){
 
 	//TODO add fops calls
 
+	//module installed, initial state OFFLINE
+	elevator.movement =OFFLINE;
+	
+	
 	INIT_LIST_HEAD(&elevator.passengers);
 	INIT_LIST_HEAD(&building.waitingForService);
 
+	//initalize the elevator locks
 	mutex_init(&elevator_mutex);
 	mutex_init(&building_mutex);
+	
+	
+	//initalize the floor lock
+	mutex_init(&floor_mutex);
+	
+	STUB_start_elevator = & (start_elevator);
+	//STUB_issue_request = & (issue_request);
+	STUB_stop_elevator = & (stop_elevator);
+	
+	
 
-	//TODO implement elevator_run
+	    //TODO implement elevator_run
         //thread_elevator = kthread_run(elevator_run, NULL, "elevator thread");
-	if(IS_ERR(thread_elevator)){
+	/*if(IS_ERR(thread_elevator)){
 		printk("Error encountered in kthread_run of elevator thread");
 		return PTR_ERR(thread_elevator);
 	}
@@ -146,14 +264,37 @@ static int elevator_init(void){
 	if(IS_ERR(thread_loader)){
 		printk("Error encountered in kthread_run of loader thread");
 		return PTR_ERR(thread_loader);
-	}
+	}*/
 
 	building.serviced = 0;
 
-	if(!proc_create(ENTRY_NAME, PERMS, NULL, &fops)){
+	/*if(!proc_create(ENTRY_NAME, PERMS, NULL, &fops)){
 		printk("ERROR IN elevator_init\n");
 		remove_proc_entry(ENTRY_NAME,NULL);
 		return -ENOMEM;
-	}
+	}*/
 	return 0;
 }
+module_init(elevator_init);
+
+
+static void elevator_exit(void)
+{
+	STUB_start_elevator = NULL;
+	//STUB_issue_request = NULL;
+	STUB_stop_elevator = NULL;
+
+	stop_elevator();
+	
+	//remove_proc_entry(ENTRY_NAME,NULL);
+	
+	mutex_destroy(&elevator_mutex);	// Destroy/Clean locks
+	mutex_destroy(&building_mutex);
+	
+	
+	printk(KERN_NOTICE "Removing /proc/%s.\n", ENTRY_NAME);
+}
+
+module_exit(elevator_exit);
+
+
---
+++ part3/elevator_calls_wrappers.c
@@ -0,0 +1,40 @@
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
+//needed in order to compile
+
+//Declares function pointers for syscalls and exports them for module usage
+long (*STUB_start_elevator)(void) = NULL;
+long (*STUB_issue_request)(int, int, int) = NULL;
+long (*STUB_stop_elevator)(void) = NULL;
+EXPORT_SYMBOL_GPL(STUB_start_elevator);
+EXPORT_SYMBOL_GPL(STUB_issue_request);
+EXPORT_SYMBOL_GPL(STUB_stop_elevator);
+
+//Start elevator wrapper
+asmlinkage long sys_start_elevator(void)
+{
+  if (STUB_start_elevator != NULL)
+    return STUB_start_elevator();
+  else
+    return -ENOSYS;
+}
+
+//Issue request wrapper
+asmlinkage long sys_issue_request(int passengerCarryType, int currentFloor, int destinationFloor)
+{
+  if (STUB_issue_request != NULL)
+    return STUB_issue_request(passengerCarryType, currentFloor, destinationFloor);
+  else
+    return -ENOSYS;
+}
+
+//Stop elevator wrapper
+asmlinkage long sys_stop_elevator(void)
+{
+  if (STUB_stop_elevator != NULL)
+    return STUB_stop_elevator();
+  else
+    return -ENOSYS;
+}
